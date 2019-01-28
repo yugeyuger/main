9682365e75366dbe3d03a1ce4f522601001cb7af
Andrew (Anrui) Chen <aan.o.chen@gmail.com>
2019-01-27 23:47:51 -0500

    show post info

--- client/src/Routes/Post.js
+++ client/src/Routes/Post.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react'
-import IndividualPost from '../Components/Post/IndividualPost'
+import React, { Component } from "react";
+import Header from "../Components/Resuable Components/Header/Header";
+import IndividualPost from "../Components/Post/IndividualPost";
 
 class Post extends Component {
-
-	render() {
-		console.log("AAAaaaaaaaaaaaaaaaaa")
-		return (
-			<div><IndividualPost/></div>
-		)
-	}
+  render() {
+    console.log("AAAaaaaaaaaaaaaaaaaa");
+    return (
+      <div>
+        <Header />
+        <IndividualPost />
+      </div>
+    );
+  }
 }
 
-export default Post
+export default Post;
--- server/server.js
+++ server/server.js
@@ -1,30 +1,50 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const jwt = require('jsonwebtoken')
-var steem = require('steem')
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+var steem = require("steem");
 
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
 
-var ServerHelpers = require('./serverHelpers.js')
-var serverHelpers =  new ServerHelpers()
+var ServerHelpers = require("./serverHelpers.js");
+var serverHelpers = new ServerHelpers();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.post('/login', async (req, res) => {
-	const { username, password } = req.body
-	var userinfo
-	try {
-		userInfo = await serverHelpers.logUserIn(username, password)
-		res.json(userInfo)
-	} catch(error) {
-		console.log(error)
-	}
-})
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  var userinfo;
+  try {
+    userInfo = await serverHelpers.logUserIn(username, password);
+    res.json(userInfo);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-app.get('/@', async (req, res) => {
-	var userProfileInfo = await serverHelpers.getUserProfileInfo(req.query.username)
-	res.json({ userProfileInfo  })
-})
+app.get("/@", async (req, res) => {
+  var userProfileInfo = await serverHelpers.getUserProfileInfo(
+    req.query.username
+  );
+  res.json({ userProfileInfo });
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.get("/@:username/:permLink", async (req, res) => {
+  var author = req.params.username;
+  var permLink = req.params.permLink;
+  //res.json({ author, permLink });
+
+  var postInfo = await serverHelpers.getPostsDataFromSteem(permLink, author);
+  res.json({ postInfo });
+
+  /*
+    steem.api
+      .getContent(author, permLink, (err, result) => {
+        //console.log(err, result);
+      })
+      .then(result => {
+        res.json({ content: result });
+      });*/
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
